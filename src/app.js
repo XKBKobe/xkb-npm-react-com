70800c75b0d7ae7cc5fad607e295dc929157b02b
xkb <xkb@xkbdeMacBook-Air.local>
2022-02-21 14:17:47 +0800

    f

--- src/app.js
+++ src/app.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Demo from './demo/SlideVerify';
 import Test from './demo/Test';
 import '@babel/polyfill'
 
 ReactDOM.render(
   <>
-  <Demo />
   <Test />
   </>,
   document.getElementById('app')
